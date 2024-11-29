5c19ed55ad4cbdc1a0fcef1586c468325ef5e594
Pitacd <2080121@student.uma.pt>
2024-11-29 11:21:17 +0000

    chore: debug show when receive msg from HTTP

--- src/order-service/routes/root.js
+++ src/order-service/routes/root.js
@@ -3,6 +3,7 @@
 module.exports = async function (fastify, opts) {
   fastify.post('/', async function (request, reply) {
     const msg = request.body
+    console.log(`[GET] Received from HTTP: ${msg}`);
     fastify.sendMessage(Buffer.from(JSON.stringify(msg)))
     reply.code(201)
   })
