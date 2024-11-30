32a4c6e99f7d90688cb4bc0700001508c899ed73
José Pita <2080121@student.uma.pt>
2024-11-30 16:28:47 +0000

    feat: made custom metrics for prometheus about gRPC

--- src/order-service/app.js
+++ src/order-service/app.js
@@ -4,8 +4,45 @@ const path = require('path');
 const AutoLoad = require('@fastify/autoload');
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
+const client = require('prom-client');
 
 module.exports = async function (fastify, opts) {
+  
+  //  ------ Prometheus Custom Metrics  ------
+
+  // Register Prometheus metrics
+  const register = new client.Registry();
+
+  // Create Prometheus metrics
+  const requestCounter = new client.Counter({
+    name: 'grpc_requests_total',
+    help: 'Total number of gRPC requests',
+    labelNames: ['method'],
+  });
+
+  const responseTimeHistogram = new client.Histogram({
+    name: 'grpc_response_time_seconds',
+    help: 'Response time for gRPC requests in seconds',
+    labelNames: ['method'],
+    buckets: [0.1, 0.5, 1, 2.5, 5, 10],
+  });
+
+  const activeRequestsGauge = new client.Gauge({
+    name: 'grpc_active_requests',
+    help: 'Number of active gRPC requests being handled',
+  });
+
+  register.registerMetric(requestCounter);
+  register.registerMetric(responseTimeHistogram);
+  register.registerMetric(activeRequestsGauge);
+
+  // Expose the metrics endpoint
+  fastify.get('/metrics', async (req, reply) => {
+    reply.header('Content-Type', register.contentType);
+    reply.send(await register.metrics());
+  });
+
+  // ------ End Prometheus Metrics Definition  ------
 
   fastify.register(require('@fastify/cors'), { origin: '*' });
 
@@ -39,8 +76,17 @@ module.exports = async function (fastify, opts) {
 
   // Implement the EnviarMensagem method
   function enviarMensagem(call, callback) {
+    
+    // Start timer for response time metric
+    const startTime = process.hrtime(); 
+    // Increment active requests gauge
+    activeRequestsGauge.inc();
+
     const mensagem = call.request;
     console.log('[GET] Received from gRPC:\n', mensagem);
+
+    // Increment request counter
+    requestCounter.inc({ method: 'EnviarMensagem' }); 
   
     let parsedConteudo;
     try {
@@ -64,6 +110,14 @@ module.exports = async function (fastify, opts) {
     callback(null, {
       resposta: `[GET] Message received with sucess: ${mensagem.conteudo}`,
     });
+
+    // Record response time
+    const [seconds, nanoseconds] = process.hrtime(startTime);
+    const durationInSeconds = seconds + nanoseconds / 1e9;
+    responseTimeHistogram.observe({ method: 'EnviarMensagem' }, durationInSeconds);
+
+    // Decrement active requests gauge
+    activeRequestsGauge.dec(); 
   }
   
   // Create gRPC server
